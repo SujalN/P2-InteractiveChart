22b27f03793b96d04bda1ce141d344a4a049013e
Josh Koh <joshuakoh225@gmail.com>
2019-09-10 06:22:30 +0100

    Added Javadoc formatting and comments.

--- Assign1SeatingChart-master/joshkoh.java
+++ Assign1SeatingChart-master/joshkoh.java
@@ -1,24 +1,24 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.lang.Math;
 /**
- * Write a description of class joshkoh here.
+ * joshkoh is a child class of student, so it inherits the attributes of the Student parent class as well.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Class joshkoh contains the instructions and procedures for student "Joshua Koh". When clicked, the student says their name and performs some type of
+ * animation. 
+ * 
+ * @author Joshua Koh
+ * @version 9/9/2019
  */
 public class joshkoh extends Student implements SpecialInterestOrHobby
 {
 
     /**
-     * Constructor for the KilgoreTrout class.
-     * Constructors are special methods with the same exact name as the class name.  
-     * Constructors to not have return types.
-     * Constructors can be overloaded. This means we can call a constructor with different sets of parameter
-     *  lists to initalize for different conditions (depending on what constructors have been written.
-     * @param String f (firstname)
-     * @param String l (lastname)
-     * @param int r (row of seating arrangement)
-     * @param int s (seat number within row seating arrangement)
+     * These are the constructors for the joshkoh class. These constructors can be overloaded, or called with different arguments. This constructor
+     * is called with the below parameters in order to initialize the joshkoh class.
+     * @param String f (Josh)
+     * @param String l (Koh)
+     * @param int r (2)
+     * @param int s (3)
      * 
      */
     public joshkoh(String f, String l, int r, int s) {
@@ -26,93 +26,132 @@ public class joshkoh extends Student implements SpecialInterestOrHobby
         lastName=l;
         myRow=r;
         mySeat=s;
-        portraitFile=f.toLowerCase()+l.toLowerCase()+".jpg";    // Make sure to name your image files firstlast.jpg, all lowercase!!!
-        standingFile=firstName.toLowerCase()+ lastName.toLowerCase()+"-standing.jpg";
-        soundFile=f.toLowerCase()+l.toLowerCase()+".wav";  // Make sure to name your sound files firstlast.wav, all lowercase!!!
+        portraitFile=f.toLowerCase()+l.toLowerCase()+".jpg";    // joshkoh.jpg
+        standingFile=firstName.toLowerCase()+ lastName.toLowerCase()+"-standing.jpg"; // joshkoh-standing.jpg
+        soundFile=f.toLowerCase()+l.toLowerCase()+".wav";  // joshkoh.wav
         redboneFile="redbone.wav";
         setImage(portraitFile);
         sitting=true;
     }
     /**
-     * Default constructor, if you don't pass in a name and seating location
-     * Pay attention to how the row and seat variables set the location of the image.  1,1 is the first cell in the upper left
-     * of the classroom.
+     * This is the constructor that the joshkoh class defaults to if no parameters are called. This causes the object to move to 1,1 and set the 
+     * portraitFile and standingFile to the default name. This constructor is similar to the above, except that the parameters are defaulted.
+     * 
+     * @param args Unused.
+     * @return Returns nothing.
      */
     public joshkoh() {
         firstName="Josh";
         lastName="Koh";
         myRow=1;
         mySeat=1;
-        // imgFile=firstName.toLowerCase()+ lastName.toLowerCase()+".jpg";
-        portraitFile=firstName.toLowerCase()+ lastName.toLowerCase()+".jpg";
-        standingFile=firstName.toLowerCase()+ lastName.toLowerCase()+"-standing.jpg";
-        soundFile=firstName.toLowerCase()+ lastName.toLowerCase()+".wav";
+        portraitFile=firstName.toLowerCase()+ lastName.toLowerCase()+".jpg"; // joshkoh.jpg
+        standingFile=firstName.toLowerCase()+ lastName.toLowerCase()+"-standing.jpg"; // joshkoh-standing.jpg
+        soundFile=firstName.toLowerCase()+ lastName.toLowerCase()+".wav"; // joshkoh.wav
         redboneFile="redbone.wav";
         setImage(portraitFile);
         sitting=true;
     }
-    
     /**
-    * Act - do whatever the KilgoreTrout actor wants to do. This method is called whenever
+    * Act - do whatever the joshkoh actor wants to do. This method is called whenever
     * the 'Act' or 'Run' button gets pressed in the environment.
+    * 
+    * @param args Unused.
+    * @return Returns nothing.
     */   
     public void act() 
     {
-        // Add your action code here.
+        // Action code
         if(Greenfoot.mouseClicked(this)){
             if (sitting){
+                // Actor stands up and says name
                 sitting=false;
                 getName();
                 sayName(soundFile);
                 
-                // Talk about my hobby
+                // My Hobby
                 myHobby("I build robots!");
                 
-                // Calls my actions
+                // Calls my action (which I called mlg)
                 mlg();
             }
             else {
+                // If the actor is already standing up and is clicked, it will answer the question.
                 answerQuestion();
+                
+                // When the question is answered, the actor will then sit down.
                 sitDown();
             }
                     
         }
     } 
-    
     /**
-     * Prints the first and last name to the console
+     * This method prints my first and last name to the console. (Josh Koh)
+     * 
+     * @param args Unused.
+     * @return Returns a print statement.
      */
     public void getName(){
         System.out.println("My name is " + firstName + " " + lastName + ".");
     }
     /**
-     * This method needs to allow the user to interact with the student through a question and answer interface, and provide
-     * some mechanism that allows the student to sit down once the Q&A session ends.  You can use this basic model, or come up
-     * with some additional class and object that represents a blackboard, or a talking cartoon bubble etc. If you provide extra
-     * classes, make sure to fully document so other students can use the same interface.
+     * This method allows the actor to interact with the teacher and answer questions. Because this happens all of the time,
+     * I will start the conversation by talking about Christy. I'm not salty, honest! I just think it's fun to allude to it.
+     * The following part is the serious part about what was hard over the summer. If the user asks what about syntax was difficult,
+     * I will elaborate. If the question is not recognized, I will not answer, and just sit down.
+     * 
+     * @param args Unused.
+     * @return Returns nothing.
      */
     public void answerQuestion(){
         String q=Greenfoot.ask("Sorry I came up so close. What would you like to know about my sister?");
         q = q.toLowerCase();
-        if (q.contains("who") || q.contains("what") || q.contains("why") || q.contains("where"))
-        {
-            q=Greenfoot.ask("Christy Koh goes to Berkeley and does EECS. " + 
-            "She's done many famous projects such as Stay On Course, Noh Snap, and Blindsight. " + 
-            "During her time at high school, she was president of the robotics club, the chemistry club, etc, etc. \n" + 
-            "Everyone who talks to me only asks about Christy anyway, so this spiel is kind of memorized. Any other questions?");
         
-            if (q.contains("Yes"))
+        q=Greenfoot.ask("Christy Koh goes to Berkeley and does EECS. " + 
+        "She's done many famous projects such as Stay On Course, Noh Snap, and Blindsight. " + 
+        "During her time at high school, she was president of the robotics club, the chemistry club, etc, etc. \n" + 
+        "Everyone who talks to me only asks about Christy anyway, so this spiel is kind of memorized. Any other questions?");
+        
+            if (q.toLowerCase().contains("hard") || q.toLowerCase().contains("difficult"))
             {
+                q=Greenfoot.ask("The only thing I found really difficult was getting used to the syntax of Java. Any other questions?");
                 
+                if (q.toLowerCase().contains("syntax") || q.toLowerCase().contains("difficult") || q.toLowerCase().contains("why"))
+                {
+                    q=Greenfoot.ask("I usually code in C++, so although the syntax was somewhat similar, I found the way that Java defines \n" +
+                    "methods was really odd. In addition, the fact that Java can't do basic integer/double arithmetic somewhat irks me. \n" + 
+                    "That's all I really have to say; I'll sit down now.");
+                }
+                else 
+                {
+                    q=Greenfoot.ask("Alright, I'll get seated.");
+                }
             }
-        }
+            else 
+            {
+                q=Greenfoot.ask("That isn't a question I can answer. I'll sit down now.");
+            }
         
-        else 
-        {
-          q=Greenfoot.ask("I don't think that was a question... I'll sit down now."); 
         }
-        
-    }
+    /**
+     * 
+     * This is the method that contains the animation for the actor. The method begins by delaying and allowing the joshkoh.wav file to finish playing,
+     * then changes the image to standingFile and plays the redbone.wav sound track.
+     * 
+     * This method also uses a 2D array to control the sizing of the image. I realized that Greenfoot would freak out if I simply plugged
+     * in x*i and y*i into the image.scale function, so I had to find a way to limit the maximum scale. This was done using a 2D array. The 2D array
+     * has length 2 and width 73, with each row corresponding to the value of i and each column corresponding to the x and y dimentions, respectively.
+     * The 2D array populates using the x*i formula until it reaches row size_limit, after which it repeats the highest dimentions so that the 
+     * image stops scaling. This allowed Greenfoot to run smoothly and without overloads.
+     * 
+     * To rotate the image, a Math.random() method is called in order to vary the degrees of rotation. This allows the rotation of the image
+     * to be silly and unpredictable.
+     * 
+     * Finally, to preserve sanity, the image returns to its original size and orientation.
+     * 
+     * @param args Unused.
+     * @return Returns nothing.
+     */
     public void mlg(){
        // Short delay to allow JoshKoh.wav to finish playing
        Greenfoot.delay(70);
@@ -175,6 +214,10 @@ public class joshkoh extends Student implements SpecialInterestOrHobby
        image.scale(100,125);
        setRotation(0);
     }
+    /**
+     * @param Requires string.
+     * @return Returns print statement.
+     */
     public void myHobby(String s) {
         System.out.println(s);
     }
